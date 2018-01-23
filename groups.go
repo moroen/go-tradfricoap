0b31cd6ade7b9bf2a639b444b7a91967de9a3067
Moroen <moroen@gmail.com>
2018-01-23 19:52:09 +0100

    Group level added

--- groups.go
+++ groups.go
@@ -10,12 +10,14 @@ import (
 )
 
 type TradfriGroup struct {
-	Id   int64
-	Name string
+	Id     int64
+	Name   string
+	State  string
+	Dimmer int64
 }
 
 func (g TradfriGroup) Describe() string {
-	return fmt.Sprintf("%d: %s", g.Id, g.Name)
+	return fmt.Sprintf("%d: %s - %s (%d)", g.Id, g.Name, g.State, g.Dimmer)
 }
 
 type TradfriGroups []TradfriGroup
@@ -26,13 +28,26 @@ func GetGroup(id int64) (TradfriGroup, error) {
 	if err != nil {
 		panic(err.Error())
 	}
-	fmt.Println(msg.String())
+
+	// fmt.Println(msg.String())
 
 	currentGroup := msg.GetBytes()
 	aGroup.Id = id
 
 	if value, err := jsonparser.GetString(currentGroup, attr_group_name); err == nil {
 		aGroup.Name = value
+	}
+
+	if value, err := jsonparser.GetInt(currentGroup, attr_light_state); err == nil {
+		if value == 1 {
+			aGroup.State = "On"
+		} else {
+			aGroup.State = "Off"
+		}
+	}
+
+	if value, err := jsonparser.GetInt(currentGroup, attr_light_dimmer); err == nil {
+		aGroup.Dimmer = value
 	}
 
 	return aGroup, nil
@@ -68,5 +83,11 @@ func GroupSetState(id int64, state int) (canopus.MessagePayload, error) {
 	uri := fmt.Sprintf("%s/%d", uri_Groups, id)
 	payload := fmt.Sprintf("{\"%s\":%d}", attr_light_state, state)
 	// fmt.Println(uri, payload)
+	return PutRequest(uri, payload)
+}
+
+func GroupSetLevel(id int64, level int) (msg canopus.MessagePayload, err error) {
+	uri := fmt.Sprintf("%s/%d", uri_Groups, id)
+	payload := fmt.Sprintf("{\"%s\":%d}", attr_light_dimmer, level)
 	return PutRequest(uri, payload)
 }
