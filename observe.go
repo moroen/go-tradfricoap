00cb2f1a520dfd7ad0769fcc9e6d1ef676b85880
Moroen <moroen@gmail.com>
2018-04-29 19:56:50 +0200

    Trying to satbilize observe

--- observe.go
+++ observe.go
@@ -6,8 +6,16 @@ import (
 	"github.com/moroen/canopus"
 )
 
-func Observe(uri string) {
-	conn, err := canopus.DialDTLS(globalGatewayConfig.Gateway, globalGatewayConfig.Identity, globalGatewayConfig.Passkey)
+func Observe(uri string, status chan bool) {
+	// fmt.Printf("Calling Observe for %s", uri)
+
+	// conn, err := canopus.DialDTLS(globalGatewayConfig.Gateway, globalGatewayConfig.Identity, globalGatewayConfig.Passkey)
+	conn, err := canopus.DialDTLS(globalGatewayConfig.Gateway, "IKEA03", "v8CCsb5bbw2qmfwU")
+	fmt.Println("Using IDENT: ", "IKEA03")
+	if err != nil {
+		panic(err.Error())
+	}
+
 	tok, err := conn.ObserveResource(uri)
 	if err != nil {
 		panic(err.Error())
@@ -29,18 +37,25 @@ func Observe(uri string) {
 					val := obsMsg.GetValue()
 
 					fmt.Println("[CLIENT >> ] Got Change Notification for resource and value: ", notifyCount, resource, val)
-					go conn.CancelObserveResource(uri, tok)
-					go conn.StopObserve(obsChannel)
-
-					obsChannel = make(chan canopus.ObserveMessage)
-					go conn.Observe(obsChannel)
+					if notifyCount == 2 {
+						fmt.Println("[CLIENT >> ] Canceling observe after 2 notifications..")
+						go conn.CancelObserveResource(uri, tok)
+						// go conn.StopObserve(obsChannel)
+						done <- true
+						return
+					}
 				} else {
-					// done <- true
-					// return
+					done <- true
+					return
 				}
 			}
 		}
 	}()
 	<-done
 	fmt.Println("Done")
+	status <- true
+	// stat := conn.Close()
+	// if stat != nil {
+	// 	panic(err.Error())
+	// }
 }
